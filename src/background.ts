3079651c7bd9c24f941b3c6bd912569eca87f2c6
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-07-22 14:38:41 -0400

    Parallel fetch locally that has access to file
    Note: using a fake URL right now. See GitHub for more info.

--- src/background.ts
+++ src/background.ts
@@ -39,20 +39,45 @@ async function generateQuery(message: { context: string, url: string, dims: numb
     });
 }
 
-function handleMessage(p: Runtime.Port, message: any) {
+function generateLocalQuery(message: { context: string, url: string, dims: number[], image: string}): IMAGERequest {
+    return {
+        "request_uuid": uuidv4(),
+        "timestamp": Math.round(Date.now() / 1000),
+        "URL": message.url,
+        "image": message.image,
+        "dimensions": message.dims,
+        "context": message.context,
+        "language": "en",
+        "capabilities": [],
+        "renderers": [
+            "ca.mcgill.a11y.image.renderer.Text",
+            "ca.mcgill.a11y.image.renderer.SimpleAudio"
+        ],
+    } as IMAGERequest;
+}
+
+async function handleMessage(p: Runtime.Port, message: any) {
+    let query: IMAGERequest;
     switch (message["type"]) {
+        case "info":
+            const value = responseMap.get(message["request_uuid"]);
+            p.postMessage(value);
+            responseMap.delete(message["request_uuid"]);
+            break;
         case "resource":
+        case "localResource":
             // Get response and open new window
-            let query: IMAGERequest;
-            generateQuery(message).then(q => {
-                query = q;
-                return fetch("https://image.a11y.mcgill.ca/render", {
+            if (message["type"] === "resource") {
+                query = await generateQuery(message);
+            } else {
+                query = generateLocalQuery(message);
+            }
+            fetch("https://image.a11y.mcgill.ca/render", {
                     "method": "POST",
                     "headers": {
                         "Content-Type": "application/json"
                     },
                     "body": JSON.stringify(query)
-                });
             }).then(resp => {
                 return resp.json();
             }).then((json: IMAGEResponse) => {
@@ -72,11 +97,6 @@ function handleMessage(p: Runtime.Port, message: any) {
             }).catch(err => {
                 console.error(err);
             });
-            break;
-        case "info":
-            const value = responseMap.get(message["request_uuid"]);
-            p.postMessage(value);
-            responseMap.delete(message["request_uuid"]);
             break;
         default:
             console.debug(message["type"]);
