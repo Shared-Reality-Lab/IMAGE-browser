090aebb1c33c209199884e57efc2ced6d1fc4102
Juliette Regimbal <juliette.regimbal@mailbox.org>
2021-07-30 14:58:59 -0400

    Very rough interface for audio segments

--- src/info/info.ts
+++ src/info/info.ts
@@ -9,7 +9,8 @@ import { IMAGEResponse } from "../types/response.schema";
 let request_uuid = window.location.search.substring(1);
 let renderings: IMAGEResponse;
 let port = browser.runtime.connect();
-port.onMessage.addListener(message => {
+const audioCtx = new window.AudioContext();
+port.onMessage.addListener(async (message) => {
     if (message) {
         renderings = message;
     } else {
@@ -29,6 +30,7 @@ port.onMessage.addListener(message => {
     for (let rendering of renderings["renderings"]) {
         let container = document.createElement("section");
         container.classList.add("container");
+        container.classList.add("rendering");
         let labelButton = document.createElement("button");
         let contentId = "m-" + uuidv4();
         labelButton.classList.add("btn", "btn-primary");
@@ -46,6 +48,7 @@ port.onMessage.addListener(message => {
             container.append(div);
             let contentDiv = document.createElement("div");
             contentDiv.classList.add("collapse");
+            contentDiv.classList.add("rendering-content");
             contentDiv.id = contentId;
             div.append(contentDiv);
             const text = rendering["data"]["text"] as string;
@@ -59,6 +62,7 @@ port.onMessage.addListener(message => {
             container.append(div);
             let contentDiv = document.createElement("div");
             contentDiv.classList.add("collapse");
+            contentDiv.classList.add("rendering-content");
             contentDiv.id = contentId;
             div.append(contentDiv);
             const audio = document.createElement("audio");
@@ -66,10 +70,87 @@ port.onMessage.addListener(message => {
             audio.setAttribute("src", rendering["data"]["audio"] as string);
             contentDiv.append(audio);
         }
+        else if (rendering["type_id"] === "ca.mcgill.a11y.image.renderer.SegmentAudio") {
+            console.log("A thing occurred!");
+            console.log(rendering["data"]["audioInfo"]);
+            // Just show basic thing for now
+            let div = document.createElement("div");
+            div.classList.add("row");
+            container.append(div);
+            let contentDiv = document.createElement("div");
+            contentDiv.classList.add("collapse");
+            contentDiv.classList.add("rendering-content");
+            contentDiv.id = contentId;
+            div.append(contentDiv);
+            const selectDiv = document.createElement("div");
+            selectDiv.classList.add("form-floating");
+            contentDiv.append(selectDiv);
+            const label = document.createElement("label");
+            label.textContent = "Part of Rendering to Play";
+            label.classList.add("form-label");
+            const select = document.createElement("select");
+            select.classList.add("form-select");
+            select.setAttribute("id", "m-" + uuidv4());
+            label.setAttribute("for", select.id);
+            const fullOption = document.createElement("option");
+            fullOption.setAttribute("value", "full");
+            fullOption.setAttribute("selected", "true");
+            fullOption.textContent = "Full Rendering";
+            select.append(fullOption);
+            const audioInfo = rendering["data"]["audioInfo"] as { "name": string, "offset": number, "duration": number }[];
+            for (let idx = 0; idx < audioInfo.length; idx++) {
+                const opt = document.createElement("option");
+                opt.setAttribute("value", idx.toString());
+                const val = audioInfo[idx];
+                opt.textContent = val["name"];
+                select.append(opt);
+            }
+            selectDiv.append(select);
+            selectDiv.append(label);
+
+            const button = document.createElement("button");
+            button.textContent = "Play Segment";
+            button.classList.add("btn", "btn-secondary");
+            selectDiv.append(button);
+
+            const audioBuffer = await fetch(rendering["data"]["audioFile"] as string).then(resp => {
+                return resp.arrayBuffer();
+            }).then(buffer => {
+                return audioCtx.decodeAudioData(buffer);
+            }).catch(e => { console.error(e); throw e; });
+
+            let currentOffset: number|undefined;
+            let currentDuration: number|undefined;
+
+            select.addEventListener("input", (e) => {
+                const evt = e as InputEvent;
+                const target = evt.target as HTMLSelectElement;
+                if (target.value === "full") {
+                    currentOffset = undefined;
+                    currentDuration = undefined;
+                } else {
+                    const idx = parseInt(target.value);
+                    const data = audioInfo[idx];
+                    currentOffset = data["offset"] as number;
+                    currentDuration = data["duration"] as number;
+                }
+            });
+            button.addEventListener("click", _ => {
+                const sourceNode = audioCtx.createBufferSource();
+                sourceNode.buffer = audioBuffer;
+                sourceNode.connect(audioCtx.destination);
+                sourceNode.start(0, currentOffset, currentDuration);
+            });
+            // const audio = document.createElement("audio");
+            // audio.setAttribute("controls", "");
+            // audio.setAttribute("src", rendering["data"]["audioFile"] as string);
+            // contentDiv.append(audio);
+        }
+
         document.body.append(container);
         count++;
     }
-    Array.from(document.getElementsByTagName("audio")).map(i => new Plyr(i));
+    //Array.from(document.getElementsByTagName("audio")).map(i => new Plyr(i));
 });
 
 port.postMessage({
