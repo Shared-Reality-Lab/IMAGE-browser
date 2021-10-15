4b494ef9ff43246b1225f8abbed92ff5bb590ace
Ben Macnaughton <robert.macnaughton@mail.mcgill.ca>
2021-10-15 12:48:37 -0400

    demoable extraction of map features

--- src/content.ts
+++ src/content.ts
@@ -6,6 +6,7 @@ let port = browser.runtime.connect();
 
 document.addEventListener("contextmenu", (evt: Event) => {
     selectedElement = evt.target as HTMLElement;
+    console.info(selectedElement.id);
 });
 
 port.onMessage.addListener(message => {
@@ -58,7 +59,14 @@ port.onMessage.addListener(message => {
             }
             break;
         case "preprocessMap":
-            
+            let mapElement: HTMLIFrameElement;
+            if (selectedElement instanceof HTMLIFrameElement) {
+                mapElement = selectedElement;
+            } else {
+                mapElement = selectedElement?.querySelector("iframe") as HTMLIFrameElement;
+            }
+            console.debug(mapElement.src);
+            break;
         default:
             console.debug(message["type"]);
             break;
@@ -70,5 +78,44 @@ port.onMessage.addListener(message => {
 Array.from(document.getElementsByTagName("img")).forEach(image => {
     if (!image.hasAttribute("tabindex") && !image.closest("a")) {
         image.setAttribute("tabindex", "0");
+    }
+});
+
+// Process maps on page
+Array.from(document.getElementsByTagName("iframe")).forEach(map => {
+    if (!map.hasAttribute("tabindex") && !map.closest("a")) {
+        if (map.hasAttribute("src") && map.src.includes("google.com/maps")) {
+            map.setAttribute("tabindex", "0");
+            let src = map.getAttribute("src");
+            let q;
+            let lat;
+            let lon;
+            let zoom;
+            let maptype = "roadmap";
+            if(src?.includes("&q=")){
+               let i1 = src.indexOf("&q=") + 3;
+               let i2 = src.indexOf("&", i1) == -1 ? src.length : src.indexOf("&", i1);
+               q = src.substring(i1, i2);
+               console.log("Q = " + q);
+            }
+            if(src?.includes("&center=")){
+                let i1 = src.indexOf("&center=") + 8;
+                let i2 = src.indexOf("&", i1) == -1 ? src.length : src.indexOf("&", i1);
+                let center = src.substring(i1, i2);
+                lat = center.split(",")[0];
+                lon = center.split(",")[1];
+                console.log("Lat = " + lat + ", Lon = " + lon);
+             }
+             if(src?.includes("&zoom=")){
+                let i1 = src.indexOf("&zoom=") + 6;
+                let i2 = src.indexOf("&", i1) == -1 ? src.length : src.indexOf("&", i1);
+                zoom = src.substring(i1, i2);
+                console.log("Zoom = " + zoom);
+             }
+             if(src?.includes("&maptype=satellite")){
+                 maptype = "satellite";
+                 console.log("Maptype = " + maptype);
+             }
+        }
     }
 });
