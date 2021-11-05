188accc616ccf2131c9c3d368ede31f8f09a62e6
Rufaro Clarisa Gapare <rufaroclarisagapare@Rufaros-MacBook-Air.local>
2021-11-04 23:15:23 -0400

    adding a close button on error window panel

--- src/background.ts
+++ src/background.ts
@@ -83,7 +83,10 @@ async function handleMessage(p: Runtime.Port, message: any) {
                     if (resp.ok) {
                         return resp.json();
                     } else {
-                       window.alert(" An error occurred while trying to connect to the IMAGE server. Please try again later. If this error persists, please contact the IMAGE team by email at atp@cim.mcgill.ca.");
+                        browser.windows.create({
+                            type: "panel",
+                            url: "errors/http_error.html"
+                        });
                         console.error(`HTTP Error ${resp.status}: ${resp.statusText}`);
                         const textContent = await resp.text();
                         console.error(textContent);
@@ -97,7 +100,10 @@ async function handleMessage(p: Runtime.Port, message: any) {
                             url: "info/info.html?" + query["request_uuid"]
                         });
                     } else {
-                     window.alert("IMAGE is unable to render this graphic. Please note that IMAGE is still in pre-release development, so only works on a limited set of graphics. For working examples, please see http://srl.mcgill.ca/image.")
+                        browser.windows.create({
+                            type: "panel",
+                            url: "errors/no_renderings.html",
+                        });
                         // throw new Error("Received no renderings from test URL!");
                     }
                 }).catch(err => {
---
+++ src/errors/closingButton.ts
@@ -0,0 +1,5 @@
+const closeButtonHttpError : Element | null = document.querySelector('#httpError');
+closeButtonHttpError?.addEventListener("click", () => window.close());
+
+const closeButtonNoRenderings : Element | null = document.querySelector('#noRenderings');
+closeButtonNoRenderings?.addEventListener("click", () => window.close());
