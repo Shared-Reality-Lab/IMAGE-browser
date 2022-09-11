54aa9da8c3d6134cc55ddebb957f7444ea3574e2
jaydeepsingh25 <jaydeep.singh@telus.com>
2022-09-11 14:51:44 -0700

    remove commented code

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -19,9 +19,5 @@ closeButton?.addEventListener("click", () => window.close());
 
 const feedbackAnchor = document.getElementById("feedback-a") as HTMLAnchorElement;
 if (feedbackAnchor) {
-    // const urlParams = new URLSearchParams(window.location.search);
-    // let request_uuid =  urlParams.get("uuid") || "";
-    // let request = urlParams.get("hash") || "";
-    // let serverUrl = urlParams.get("serverURL") || "";
     feedbackAnchor.href += window.location.search;
 }
