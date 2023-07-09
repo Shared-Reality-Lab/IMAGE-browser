fe5c38560e7971f4b741563167de33fbde5bba51
Antoine Phan <antoine.phan@mail.mcgill.ca>
2023-07-09 17:24:41 -0400

    Fixed inconsistency in errors.ts

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -14,7 +14,9 @@
  * and our Additional Terms along with this program.
  * If not, see <https://github.com/Shared-Reality-Lab/IMAGE-browser/LICENSE>.
  */
-import browser from "webextension-polyfill";
+import { queryLocalisation } from "../utils";
+
+queryLocalisation();
 
 const closeButton : Element | null = document.getElementById('closingButton');
 closeButton?.addEventListener("click", () => window.close());
@@ -22,15 +24,4 @@ closeButton?.addEventListener("click", () => window.close());
 const feedbackAnchor = document.getElementById("feedbackFormLink") as HTMLAnchorElement;
 if (feedbackAnchor) {
     feedbackAnchor.href += window.location.search;
-}
-
-// Set up localized names: getting all elements with class "localisation"
-const localisation = Array.from(document.querySelectorAll(".localisation"));
-for (let label of localisation) {
-  const val = browser.i18n.getMessage(label.id);
-  if (val) {
-    label.textContent = val;
-  } else {
-    console.warn('Unknown element "' + label.id + '"');
-  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -17,7 +17,7 @@ export function getAllStorageSyncData() {
       processItem: "",
       requestItem: "",
       mweItem: "",
-      language: ""
+      language: "auto"
     });
 };
 
